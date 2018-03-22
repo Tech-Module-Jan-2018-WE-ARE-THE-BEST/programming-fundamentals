6b1c41a4a8376a899852c689e0f3c5890c04158d
Stoyan <s.chepov@abv.bg>
2018-03-22 22:38:57 +0200

    redaction

--- intro-and-basic-syntax/exercise/CharacterStats-5.cs
+++ intro-and-basic-syntax/exercise/CharacterStats-5.cs
@@ -6,12 +6,23 @@ namespace CharacterStats
     {
         static void Main(string[] args)
         {
+            //We receive from the Console the name of the video game character
             string name = Console.ReadLine();
+            /*                     
+             In the next 4 lines we receive the current health of the character and his maximal health in numbers;
+             his current and maximal energy. 
+             */
             int currentHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
-            
+
+            /*
+             * We are almost ready!
+             * We should only print the results.
+             * The handlebars {} corespond to the variable that we have to print.
+             * You may also want to use "Name: {name}" and it should work fine.
+             * */
             Console.WriteLine("Name: {0}", name);
             Console.WriteLine("Health: |{0}{1}|", new string('|', currentHealth), new string('.', maxHealth - currentHealth));
             Console.WriteLine("Energy: |{0}{1}|", new string('|', currentEnergy), new string('.', maxEnergy - currentEnergy));
