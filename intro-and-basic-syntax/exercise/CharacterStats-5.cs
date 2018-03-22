40ca62744a8712d42f8091ae2e2271a593d64999
Svetoslav Nedelchev <sssvetoslavvv@gmail.com>
2018-03-22 23:32:52 +0200

    Changed variable names in solution 5

--- intro-and-basic-syntax/exercise/CharacterStats-5.cs
+++ intro-and-basic-syntax/exercise/CharacterStats-5.cs
@@ -8,15 +8,15 @@ namespace CharacterStats
         static void Main(string[] args)
         {
             //We receive from the Console the name of the video game character
-            string name = Console.ReadLine();
+            string input = Console.ReadLine();
             /*                     
              In the next 4 lines we receive the current health of the character and his maximal health in numbers;
              his current and maximal energy. 
              */
             int currentHealth = int.Parse(Console.ReadLine());
-            int maxHealth = int.Parse(Console.ReadLine());
+            int maximumHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
-            int maxEnergy = int.Parse(Console.ReadLine());
+            int maximumEnergy = int.Parse(Console.ReadLine());
 
             /*
              * We are almost ready!
@@ -24,9 +24,9 @@ namespace CharacterStats
              * The handlebars {} corespond to the variable that we have to print.
              * You may also want to use "Name: {name}" and it should work fine.
              * */
-            Console.WriteLine("Name: {0}", name);
-            Console.WriteLine("Health: |{0}{1}|", new string('|', currentHealth), new string('.', maxHealth - currentHealth));
-            Console.WriteLine("Energy: |{0}{1}|", new string('|', currentEnergy), new string('.', maxEnergy - currentEnergy));
+            Console.WriteLine("Name: {0}", input);
+            Console.WriteLine("Health: |{0}{1}|", new string('|', currentHealth), new string('.', maximumHealth - currentHealth));
+            Console.WriteLine("Energy: |{0}{1}|", new string('|', currentEnergy), new string('.', maximumEnergy - currentEnergy));
         }
     }
 }
