14422c81aa91dac3a5c2b1badf4223b2eb9ff085
Stoyan <s.chepov@abv.bg>
2018-03-22 23:41:00 +0200

    I changed variable result to area in problem 2.cs

--- intro-and-basic-syntax/exercise/Program 2.cs
+++ intro-and-basic-syntax/exercise/Program 2.cs
@@ -10,9 +10,9 @@ namespace Problem_2._Rectangle_Area
             var width = double.Parse(Console.ReadLine());
             var height = double.Parse(Console.ReadLine());
 			//calculating the result
-            var result = (width * height);
+            var area = (width * height);
 			//writing the result 
-            Console.WriteLine($"{result:F2}");
+            Console.WriteLine($"{area:F2}");
         }
     }
 }
